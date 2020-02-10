34ccb03844dc8a05c3753a57ece1c5333647034c
Marco Kühbauch <marcokuehbauch@gmail.com>
2020-02-10 20:59:54 +0100

    move data fetching logic in custom hook and seperate reducer

---
+++ src/hooks/useGoogleBooksApi.js
@@ -0,0 +1,35 @@
+import { useState, useEffect, useReducer } from 'react';
+import axios from 'axios';
+
+import dataFetchReducer from '../reducer/dataFetchReducer';
+import { IS_LOADING, IS_SUCCESS, IS_ERROR } from '../constants/searchTypes';
+
+const useGoogleBooksAPI = () => {
+	const [url, setUrl] = useState('');
+
+	const [state, dispatch] = useReducer(dataFetchReducer, {
+		isLoading: false,
+		isError: false,
+		data: null
+	});
+
+	useEffect(() => {
+		if (url === '') return;
+
+		const getData = async () => {
+			dispatch({ type: IS_LOADING });
+			try {
+				const { data } = await axios.get(url);
+				dispatch({ type: IS_SUCCESS, payload: data });
+			} catch (error) {
+				dispatch({ type: IS_ERROR });
+			}
+		};
+
+		getData();
+	}, [url]);
+
+	return [state, setUrl];
+};
+
+export default useGoogleBooksAPI;
---
+++ src/reducer/dataFetchReducer.js
@@ -0,0 +1,29 @@
+import { IS_LOADING, IS_SUCCESS, IS_ERROR } from '../constants/searchTypes';
+
+const dataFetchReducer = (state, action) => {
+	switch (action.type) {
+		case IS_LOADING:
+			return {
+				...state,
+				isLoading: true,
+				isError: false
+			};
+		case IS_SUCCESS:
+			return {
+				...state,
+				isLoading: false,
+				isError: false,
+				data: action.payload
+			};
+		case IS_ERROR:
+			return {
+				...state,
+				isLoading: false,
+				isError: true
+			};
+		default:
+			throw new Error();
+	}
+};
+
+export default dataFetchReducer;
