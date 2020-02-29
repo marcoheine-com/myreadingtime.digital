f93022c85816351f3dd5095e7bb473d3fc856e81
Marco Kühbauch <marcokuehbauch@gmail.com>
2020-02-29 16:40:38 +0100

    implement search on browser back

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import Results from '../Results';
 import useGoogleBooksAPI from '../../hooks/useGoogleBooksApi';
@@ -11,34 +12,42 @@ export const Main = styled.main`
 `;
 
 const App = () => {
-  const [query, setQuery] = useState('Harry Potter');
+  const [query, setQuery] = useState('');
   const [searchQuery, setSearchQuery] = useState('');
   const [index, setIndex] = useState(0);
 
   const { state, setUrl } = useGoogleBooksAPI();
+  const { isLoading, isError, data } = state;
+  const location = useLocation();
+  const history = useHistory();
+
+  const handleSubmit = event => {
+    setSearchQuery(query);
+    event.preventDefault();
+    history.push(`?q=${query}&${START_INDEX}=${index}`);
+  };
 
   const handleOnclick = () => {
     setIndex(index + 10);
-    setUrl(`${API_BASE_URL}?q=${query}&${START_INDEX}=${index}`);
+    history.push(`?q=${query}&${START_INDEX}=${index}`);
   };
 
-  const { isLoading, isError, data } = state;
+  useEffect(() => {
+    if (location.search === '') return;
+
+    setUrl(`${API_BASE_URL}${location.search}`);
+  }, [location.search, setUrl]);
 
   return (
     <Main>
       <h1>myreadingtime.digital</h1>
       <p>Search for a book:</p>
-      <form
-        onSubmit={event => {
-          setUrl(`${API_BASE_URL}?q=${query}&${START_INDEX}=${index}`);
-          setSearchQuery(query);
-          event.preventDefault();
-        }}
-      >
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
           value={query}
           onChange={event => setQuery(event.target.value)}
+          placeholder='Harry Potter'
         />
         <button type='submit' disabled={query === ''}>
           Search
@@ -53,7 +62,9 @@ const App = () => {
         data && (
           <>
             <Results data={data} searchQuery={searchQuery} />
-            <button onClick={handleOnclick}>Load more books</button>
+            <button onClick={handleOnclick} disabled={query === ''}>
+              Load more books
+            </button>
           </>
         )
       )}
--- src/components/SearchPage/SearchPage.test.js
+++ src/components/SearchPage/SearchPage.test.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import SearchPage from './SearchPage';
 
 test('renders app title', () => {
-  const { getByText } = render(<SearchPage />);
+  const history = createMemoryHistory();
+  const { getByText } = render(
+    <Router history={history}>
+      <SearchPage />
+    </Router>
+  );
   expect(getByText('myreadingtime.digital')).toBeInTheDocument();
 });
 
-test.skip('renders input field with default text', () => {
-  const { getByPlaceholderText } = render(<SearchPage />);
+test('renders input field with default text', () => {
+  const history = createMemoryHistory();
+  const { getByPlaceholderText } = render(
+    <Router history={history}>
+      <SearchPage />
+    </Router>
+  );
   expect(getByPlaceholderText('Harry Potter')).toBeInTheDocument();
 });
