7c49db2efb435940458df98f297328b985f8c649
Marco Kühbauch <marcokuehbauch@gmail.com>
2021-03-13 16:52:36 +0100

    remove first part of new api from redux code

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,61 +1,37 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
-import { loadState, saveState } from '../utils/localStorage';
-
-export const wantToReadSlice = createSlice({
-  name: 'wantToRead',
-  initialState: [],
-  reducers: {
-    addToWantToRead: (state, action) => {
-      const { id, authors, smallThumbnail, title } = action.payload;
-      if (state.find((item) => item.id === id)) return state;
-      state.push({ id, authors, smallThumbnail, title });
-    },
-    removeFromWantToRead: (state, action) => {
-      const id = action.payload;
-
-      return state.filter((item) => item.id !== id);
-    },
-  },
-});
-
-export const {
-  addToWantToRead,
-  removeFromWantToRead,
-} = wantToReadSlice.actions;
+import { createSlice, configureStore } from '@reduxjs/toolkit'
+import { loadState, saveState } from '../utils/localStorage'
 
 export const didReadSlice = createSlice({
   name: 'didRead',
   initialState: [],
   reducers: {
     addToDidRead: (state, action) => {
-      const { id, authors, smallThumbnail, title } = action.payload;
-      if (state.find((item) => item.id === id)) return state;
-      state.push({ id, authors, smallThumbnail, title });
+      const { id, authors, smallThumbnail, title } = action.payload
+      if (state.find((item) => item.id === id)) return state
+      state.push({ id, authors, smallThumbnail, title })
     },
     removeFromDidRead: (state, action) => {
-      const id = action.payload;
-      return state.filter((item) => item.id !== id);
+      const id = action.payload
+      return state.filter((item) => item.id !== id)
     },
   },
-});
+})
 
-export const { addToDidRead, removeFromDidRead } = didReadSlice.actions;
+export const { addToDidRead, removeFromDidRead } = didReadSlice.actions
 
-const persistedState = loadState();
+const persistedState = loadState()
 
 const store = configureStore({
   reducer: {
-    wantToRead: wantToReadSlice.reducer,
     didRead: didReadSlice.reducer,
   },
   preloadedState: persistedState,
-});
+})
 
 store.subscribe(() => {
   saveState({
-    wantToRead: store.getState().wantToRead,
     didRead: store.getState().didRead,
-  });
-});
+  })
+})
 
-export default store;
+export default store
