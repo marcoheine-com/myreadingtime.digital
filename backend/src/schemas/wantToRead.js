d67940abf7ce1d0647d39de1a02efeb4c9e713f9
Marco Kühbauch <marcokuehbauch@gmail.com>
2021-03-13 13:56:57 +0100

    delete one should return all results

--- backend/src/schemas/wantToRead.js
+++ backend/src/schemas/wantToRead.js
@@ -4,51 +4,67 @@ const findAll = {
       type: 'array',
       items: {
         properties: {
-          bookId: { type: 'number' },
+          bookId: { type: 'string' },
           authors: { type: 'array' },
-          thumbnail: { type: 'string'},
-          title: { type: 'string' }
-        }
-      }
-    }
-  }
-}
+          thumbnail: { type: 'string' },
+          title: { type: 'string' },
+        },
+      },
+    },
+  },
+};
 
 const replaceOne = {
   body: {
     type: 'object',
     properties: {
-      userId: { type: 'string'},
-      bookId: { type: 'number' },
+      userId: { type: 'string' },
+      bookId: { type: 'string' },
       authors: { type: 'array' },
-      thumbnail: { type: 'string'},
-      title: { type: 'string' }
-    }
+      thumbnail: { type: 'string' },
+      title: { type: 'string' },
+    },
   },
   params: {
     type: 'object',
     properties: {
-      bookId: { type: 'number' }
-    }
+      bookId: { type: 'string' },
+    },
   },
-}
+  response: {
+    200: {},
+  },
+};
 
 const deleteOne = {
   params: {
     type: 'object',
     properties: {
-      bookId: { type: 'number' }
-    }
-  }
-}
+      bookId: { type: 'string' },
+    },
+  },
+  response: {
+    200: {
+      type: 'array',
+      items: {
+        properties: {
+          bookId: { type: 'string' },
+          authors: { type: 'array' },
+          thumbnail: { type: 'string' },
+          title: { type: 'string' },
+        },
+      },
+    },
+  },
+};
 
 const health = {
   params: {
     type: 'object',
     properties: {
-      healthCheck: { type: 'string'}
-    }
-  }
-}
+      healthCheck: { type: 'string' },
+    },
+  },
+};
 
-module.exports = { findAll, replaceOne, deleteOne, health }
+module.exports = { findAll, replaceOne, deleteOne, health };
