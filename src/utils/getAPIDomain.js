a1e707ab4260b8302bbd6d43882cbc183de373d0
Marco Kühbauch <marcokuehbauch@gmail.com>
2021-03-20 15:18:23 +0100

    change env var

--- src/utils/getAPIDomain.js
+++ src/utils/getAPIDomain.js
@@ -1,4 +1,4 @@
 export const getAPIDomain = () =>
   process.env.NODE_ENV === 'production'
-    ? process.env.HEROKU_APP_DOMAIN
+    ? process.env.REACT_APP_HEROKU_DOMAIN
     : window.location.origin
