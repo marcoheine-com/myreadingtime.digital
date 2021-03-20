5513d8f2a7f8d3b830f45900c270767e8e8c756e
Marco Kühbauch <marcokuehbauch@gmail.com>
2021-03-20 15:09:12 +0100

    change env check

--- src/utils/getAPIDomain.js
+++ src/utils/getAPIDomain.js
@@ -1,4 +1,4 @@
 export const getAPIDomain = () =>
-  process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === 'production'
     ? process.env.HEROKU_APP_DOMAIN
     : window.location.origin
